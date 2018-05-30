7e3c0102774fd5b58a6c905532099dc386f28e48
ARON PAYLE <AronP97@hotmail.com>
2018-05-30 02:29:13 +0200

    Web Development Introduction POE. Covered HTML5, CSS3, JavaScript and Bootstrap

---
+++ Project/javascript/_vti_cnf/modernizr.custom.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|06 Mar 2015 08:51:48 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|ARON\\Aron
+vti_modifiedby:SR|ARON\\Aron
+vti_timecreated:TR|06 Mar 2015 08:51:48 -0000
+vti_cacheddtm:TX|06 Mar 2015 08:51:48 -0000
+vti_filesize:IR|5329
+vti_backlinkinfo:VX|
---
+++ Project/js/_vti_cnf/ie10-viewport-bug-workaround.js
@@ -0,0 +1,12 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TX|20 Apr 2016 21:08:26 -0000
+vti_author:SR|STD\\15010369
+vti_modifiedby:SR|STD\\15010369
+vti_nexttolasttimemodified:TX|20 Apr 2016 21:08:26 -0000
+vti_timecreated:TR|17 May 2016 07:46:02 -0000
+vti_cacheddtm:TX|17 May 2016 07:46:04 -0000
+vti_filesize:IR|641
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|rates.html booking-enquiries.html accommodation.html things-to-do.html gallery.html thank-you.html facilities.html template.html contact_us.html index.html
+vti_syncofs_localhost\\f\:\\waverly guest house\\expressiveweb code library\\standard template framework/f\:/waverly guest house/expressiveweb code library/standard template framework:TX|20 Apr 2016 21:08:26 -0000
+vti_syncwith_localhost\\f\:\\waverly guest house\\expressiveweb code library\\standard template framework/f\:/waverly guest house/expressiveweb code library/standard template framework:TW|17 May 2016 07:46:04 -0000
